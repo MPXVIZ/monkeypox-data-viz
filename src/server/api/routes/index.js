2e70b7ac447311c10b01fe3fdb00798a598bd1c0
a-hsu <a.hsu873@gmail.com>
2022-08-02 11:19:37 -0400

    mapbox react boiler init

---
+++ src/server/api/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const router = express.Router()
+
+// mount auth routes at /auth
+// router.use("/auth", authRoutes)
+
+// mount user routes at /users
+// router.use("/users", userRoutes)
+
+module.exports = router
