1dd79d97c06ebf9c612de00935339e00e44b8cff
a-hsu <a.hsu873@gmail.com>
2022-08-09 11:10:53 -0400

    basic redis middleware testing

--- src/server/api/middleware/updateRedisData.unit.test.ts
+++ src/server/api/middleware/updateRedisData.unit.test.ts
@@ -4,7 +4,10 @@ const {
     getLatestCaseData,
     updateRedisWithNewData,
 } = require('./updateRedisData');
-
+describe('updateRedisAndGetMPXData', () => {
+    it.todo('should upsert redis with all cases');
+    it.todo('should pass cases to request');
+ })
 describe('getRecentCommitHash', () => {
     it.todo('should get the most recent hash');
     it.todo('should store the hash into the request');
@@ -12,11 +15,14 @@ describe('getRecentCommitHash', () => {
 describe('upsertCommitHashToRedis', () => {
     it.todo('should check to see if a hash exists in redis');
     it.todo('should add the hash if it does not exist');
-    it.todo('should update the redis cache if the hashes are not the same');
+    it.todo('should update the redis cache with the new hash and last updated time if the hashes are not the same');
 });
 
 describe('getLatestCaseData', () => {
-    it.todo('should fetch the json file');
+    it.todo('should fetch the json file if new hash');
     it.todo('should parse the data into a json');
+    it.todo('should get redis data if old hash');
     it.todo('should store data in request');
 });
+
+// describe('updateRedisWithNewData')
