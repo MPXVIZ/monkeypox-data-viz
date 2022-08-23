11dc6c7cb0217f3d2185f0c11aaae83126fc23a7
Dylan <dylanlovespie@gmail.com>
2022-08-23 00:30:21 -0400

    added about page

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 // import WorldTimeline from './components/WorldTimeline';
 import Main from './pages/Main';
+import About from './pages/About';
 
 import './style.scss';
 
@@ -13,6 +14,10 @@ const App = () => {
                     exact
                     path="/"
                     component={Main}
+                ></Route>
+                <Route
+                    path="/about"
+                    component={About}
                 ></Route>
             </Switch>
         </Router>
