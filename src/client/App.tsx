7a7340f5d4a2bb23e1351b79d63db98cd849f7b4
Dylan <dylanlovespie@gmail.com>
2022-08-25 16:29:38 -0400

    added basic navbar

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,24 +3,28 @@ import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 // import WorldTimeline from './components/WorldTimeline';
 import Main from './pages/Main';
 import About from './pages/About';
+import Navbar from "./components/Navbar"
 
 import './style.scss';
 
 const App = () => {
     return (
-        <Router>
-            <Switch>
-                <Route
-                    exact
-                    path="/"
-                    component={Main}
-                ></Route>
-                <Route
-                    path="/about"
-                    component={About}
-                ></Route>
-            </Switch>
-        </Router>
+        <>
+            <Router>
+        <Navbar />
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        component={Main}
+                        ></Route>
+                    <Route
+                        path="/about"
+                        component={About}
+                        ></Route>
+                </Switch>
+            </Router>
+        </>
     );
 };
 
---
+++ src/client/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>
+                MonkeyPox Progression Map
+            </h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Sources</li>
+            </ul>
+        </nav>
+    );
+}
+export default Navbar;
