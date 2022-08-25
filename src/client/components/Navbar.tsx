1304793532524d68d06e9642fff67a2659afc3f7
Dylan <dylanlovespie@gmail.com>
2022-08-25 16:41:32 -0400

    fixed alignment on links

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -7,12 +7,15 @@ function Navbar() {
             <h1>
                 MonkeyPox Progression Map
             </h1>
+            <div id="links">
+
             <ul>
                 
                 <Link to="/"><li>Home</li></Link>
                 <Link to="/about"><li>About</li></Link>
-                <li>Sources</li>
+                <Link to="/sources"><li>Sources</li></Link>
             </ul>
+            </div>
         </nav>
     );
 }
